662671d89b9ad4649459cd76912798dcdebe556d
scahyono <scahyono@gmail.com>
2014-07-17 15:15:51 +0200

    Travis to use separate browser to launch parallel requests

--- src/test/java/org/cfwheels/cfwheels/CFWheelsCoreIT.java
+++ src/test/java/org/cfwheels/cfwheels/CFWheelsCoreIT.java
@@ -126,7 +126,7 @@ public class CFWheelsCoreIT {
 					"ALTER TABLE #loc.i# MODIFY COLUMN <cfif loc.i IS \"photogalleries\">photogalleryid<cfelseif loc.i IS \"photogalleryphotos\">photogalleryphotoid<cfelse>id</cfif> #loc.identityColumnType# DEFAULT #loc.seq#.nextval");
 			Files.write(Paths.get("wheels/tests/populate.cfm"), content.getBytes());
 		}
-		hitHomepageWithParalelRequest();
+		hitHomepageWithParallelRequest();
 		System.out.println("test database re-create");
 		driver.get(baseUrl + "index.cfm?controller=wheels&action=wheels&view=tests&type=core&reload=true&package=controller.caching");
 		String postfix = "";
@@ -158,7 +158,7 @@ public class CFWheelsCoreIT {
 		}
 	}
 	
-	private static void hitHomepageWithParalelRequest() {
+	private static void hitHomepageWithParallelRequest() {
 		new HitterThread().start();
 		new HitterThread().start();
 	}
