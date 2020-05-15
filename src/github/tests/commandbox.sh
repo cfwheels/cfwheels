b54f7f74056d51b6801b2919073b17168a62e203
Adam Chapman <adam.p.chapman@gmail.com>
2020-05-15 11:22:54 +1000

    try commandbox again

--- src/github/tests/commandbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo apt install libappindicator-dev openjdk-11-jre -y
-curl -fsSl https://downloads.ortussolutions.com/debs/gpg | sudo apt-key add -
-echo "deb https://downloads.ortussolutions.com/debs/noarch /" | sudo tee -a /etc/apt/sources.list.d/commandbox.list
-sudo apt update -y -qq
-sudo apt install apt-transport-https commandbox -y
