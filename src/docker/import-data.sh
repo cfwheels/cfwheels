81450b72b0f99ee07302c9f0bd4f70802aac39dd
Tom King <admin@oxalto.co.uk>
2020-05-16 17:05:46 +0100

    Another attempt to get SQLServer to live

--- src/docker/import-data.sh
+++ src/docker/import-data.sh
@@ -1,1 +1,9 @@
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P x!bsT8t60yo0cTVTPq -d master -i setup.sql
+echo Running import-data.sh
+
+echo Sleep now
+#wait for the SQL Server to come up
+sleep 60s
+
+echo Wake up
+#run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P x!bsT8t60yo0cTVTPq -d master -i /usr/work/setup.sql
