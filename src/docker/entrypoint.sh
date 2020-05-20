16b0dfde848345f05f9fbc2360c58677875d564d
Adam Chapman <adam.p.chapman@gmail.com>
2020-05-20 18:00:57 +1000

    revert sneaky ampersand

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -1,3 +1,3 @@
 echo Running entrypoint.sh
 #start SQL Server, start the script to create the DB and import the data, start the app
-/usr/work/import-data.sh && /opt/mssql/bin/sqlservr
+/usr/work/import-data.sh & /opt/mssql/bin/sqlservr
