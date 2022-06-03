0c8189885393d1d1aaa1dd08c2ea1a8895cd3288
Peter Amiri <peter@alurium.com>
2022-06-03 07:51:22 -0700

    Exclude H2 database from Adobe server options

--- src/docker/test-ui/src/services/testsuites.js
+++ src/docker/test-ui/src/services/testsuites.js
@@ -5,6 +5,10 @@ let testsuites = []
 
 for (var s = 0; s < servers.length; s++){
 	for (var d = 0; d < databases.length; d++){
+		if ((servers[s].servername == 'Adobe 2016' && databases[d].databasename == 'H2') ||
+			(servers[s].servername == 'Adobe 2018' && databases[d].databasename == 'H2')) {
+			break;
+		}
 		testsuites.push({
 			id: servers[s].server + '_' + databases[d].database,
 			displayname: servers[s].servername + ' ' + databases[d].databasename,
