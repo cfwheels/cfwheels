f57046fc3e6e79157c63aed0dd8823c10fbeb19c
Peter Amiri <peter@alurium.com>
2022-10-11 08:47:05 -0700

    rename testui to match service name

---
+++ docker/testui/src/services/servers.js
@@ -0,0 +1,24 @@
+const servers =
+[
+	{
+		servername: 'Lucee 5',
+		server: 'http://localhost:60005'
+	},
+	{
+		servername: 'Lucee 6',
+		server: 'http://localhost:60006'
+	},
+	{
+		servername: 'Adobe 2016',
+		server: 'http://localhost:62016'
+	},
+	{
+		servername: 'Adobe 2018',
+		server: 'http://localhost:62018'
+	},
+	{
+		servername: 'Adobe 2021',
+		server: 'http://localhost:62021'
+	}
+]
+export default servers
