1954a35dd764e29ba5cd7d2f3ef1731d2e53f8b1
Peter Amiri <peter@alurium.com>
2022-10-11 07:37:06 -0700

    by pass H2 for Adobe 2021

--- src/docker/test-ui/src/services/testsuites.js
+++ src/docker/test-ui/src/services/testsuites.js
@@ -6,7 +6,8 @@ let testsuites = []
 for (var s = 0; s < servers.length; s++){
 	for (var d = 0; d < databases.length; d++){
 		if ((servers[s].servername == 'Adobe 2016' && databases[d].databasename == 'H2') ||
-			(servers[s].servername == 'Adobe 2018' && databases[d].databasename == 'H2')) {
+			(servers[s].servername == 'Adobe 2018' && databases[d].databasename == 'H2') ||
+			(servers[s].servername == 'Adobe 2021' && databases[d].databasename == 'H2')) {
 			break;
 		}
 		testsuites.push({
