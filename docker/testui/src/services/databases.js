2e0528b7eb0b09daad75cac40ed2459b725befb6
Peter Amiri <peter@alurium.com>
2022-10-11 08:47:01 -0700

    rename testui to match service name

---
+++ docker/testui/src/services/databases.js
@@ -0,0 +1,19 @@
+const databases = [
+	{
+		databasename: 'MySQL',
+		database: 'mysql'
+	},
+	{
+		databasename: 'SQL Server',
+		database: 'sqlserver'
+	},
+	{
+		databasename: 'PostgreSQL',
+		database: 'postgres'
+	},
+	{
+		databasename: 'H2',
+		database: 'h2'
+	}
+]
+export default databases
