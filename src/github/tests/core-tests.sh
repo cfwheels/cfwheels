8cd884f098db6f61e687696d5709eb1d4bb5cf3d
Peter Amiri <peter@alurium.com>
2022-11-18 12:03:20 -0800

    modify port

--- src/github/tests/core-tests.sh
+++ src/github/tests/core-tests.sh
@@ -8,7 +8,7 @@ dbengine=${2}
 port="$(get_port ${cfengine})"
 db="$(get_db ${dbengine})"
 
-test_url="http://127.0.0.1:${port}/wheels/tests/core?db=${db}&format=txt&only=failure,error&reload=true"
+test_url="http://127.0.0.1:8080/wheels/tests/core?db=${db}&format=txt&only=failure,error&reload=true"
 result_file="/tmp/${cfengine}-${db}-result.txt"
 
 echo "\nRUNNING SUITE (${cfengine}/${dbengine}):\n"
