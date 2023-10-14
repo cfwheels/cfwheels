03716b0d10852cd827fbc255d8433e073535bc9d
Peter Amiri <peter@alurium.com>
2023-10-14 13:44:12 -0700

    update the local Docker test suite for Adobe 2023

--- docker/testui/src/services/servers.js
+++ docker/testui/src/services/servers.js
@@ -19,6 +19,10 @@ const servers =
 	{
 		servername: 'Adobe 2021',
 		server: 'http://localhost:62021'
+	},
+	{
+		servername: 'Adobe 2023',
+		server: 'http://localhost:62023'
 	}
 ]
 export default servers
